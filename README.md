0964f4818b2a38fca608373483d145f71bf6e3a5
Tom Viner <tom.viner@hogarthww.com>
2016-03-03 22:04:43 +0000

    final

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # micropython-wifi-chip
 Coding with the ESP8266
+
+Server code: https://github.com/shun-liang/micropython-server
--- client.py
+++ client.py
@@ -1,31 +1,36 @@
 """
 To paste this to the board:
-  - do a regex replace s/([;:])\n    /\1 /
   - copy
-  - pasta in the picocom shell!
+  - in the picocom shell:
+    - ctrl-e
+    - pasta
+    - ctrl-d
 """
 import socket
 import machine
 import neopixel
 import json
+import time
 
 def get():
-    s = socket.socket();
-    s.connect(socket.getaddrinfo('stark-oasis-75010.herokuapp.com', 80)[0][-1]);
-    s.send('GET / HTTP/1.0\r\n');
-    s.send('Host: stark-oasis-75010.herokuapp.com\r\n');
-    s.send('\r\n');
-    res = s.recv(1000);
-    hds, body = res.split('\r\n\r\n');
-    data = json.loads(body);
-    print(data);
+    s = socket.socket()
+    s.connect(socket.getaddrinfo('stark-oasis-75010.herokuapp.com', 80)[0][-1])
+    s.send('GET /get HTTP/1.0\r\n')
+    s.send('Host: stark-oasis-75010.herokuapp.com\r\n')
+    s.send('\r\n')
+    res = s.recv(1000)
+    hds, body = res.split(b'\r\n\r\n', 1)
+    data = json.loads(body)
+    print(data)
     return data
 
-on = [list((0, 0, 0)) for _ in range(8)];
-off = [list((255, 255, 255)) for _ in range(8)];
+on = [list((0, 0, 0)) for _ in range(8)]
+off = [list((255, 255, 255)) for _ in range(8)]
 
 p = machine.Pin(4, machine.Pin.OUT)
 np = neopixel.NeoPixel(p, 8)
-data = get()
-[np.__setitem__(i, d) for i,d in enumerate(data)]
-np.write()
+while 1:
+    data = get()
+    [np.__setitem__(i, d) for i,d in enumerate(data)]
+    np.write()
+    time.sleep(0.5)
